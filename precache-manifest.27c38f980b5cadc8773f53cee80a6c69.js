ddf0f52a8d7efe5712f72936694a1b0cf8bd2dbc
Кабалин Ростислав Сергеевич <rskabali@mts.ru>
2019-12-10 22:03:00 +0300

    Math-algorithms

---
+++ precache-manifest.27c38f980b5cadc8773f53cee80a6c69.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "c94eca379b24f46620cd",
+    "url": "/math-algorithms/js/about.99d10430.js"
+  },
+  {
+    "revision": "aba1114598c20c6879f1",
+    "url": "/math-algorithms/css/app.b2daca11.css"
+  },
+  {
+    "revision": "aba1114598c20c6879f1",
+    "url": "/math-algorithms/js/app.876d1857.js"
+  },
+  {
+    "revision": "db84f66315f212617914",
+    "url": "/math-algorithms/css/chunk-vendors.723a90c8.css"
+  },
+  {
+    "revision": "db84f66315f212617914",
+    "url": "/math-algorithms/js/chunk-vendors.38370435.js"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/math-algorithms/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/math-algorithms/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "7ccf57a6cd07ef1ff259d4bb878cb4b8",
+    "url": "/math-algorithms/index.html"
+  },
+  {
+    "revision": "f96046faf3e5042708c947231b1c77fd",
+    "url": "/math-algorithms/img/1.png"
+  },
+  {
+    "revision": "d84f29bee95da8def3fcbb6dbb5d8f74",
+    "url": "/math-algorithms/img/2.png"
+  },
+  {
+    "revision": "491c56cc619e646ed81a7bf186f02806",
+    "url": "/math-algorithms/img/3.png"
+  },
+  {
+    "revision": "d201fe91021f307973a3313406b56413",
+    "url": "/math-algorithms/img/4.png"
+  },
+  {
+    "revision": "fe7677894b9fac29581d9127a6b0dbe9",
+    "url": "/math-algorithms/img/5.png"
+  },
+  {
+    "revision": "4bf40d24c2a00060df65725f2dce1619",
+    "url": "/math-algorithms/img/pngtree-people-characters-avatars-set-vector-cartoon-flat-isolated-illustration-png-image_1885900.jpg"
+  },
+  {
+    "revision": "ec51b1796e65b1425cf0d63404340ca5",
+    "url": "/math-algorithms/img/6.png"
+  }
+];
